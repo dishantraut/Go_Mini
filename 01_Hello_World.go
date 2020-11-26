4190ede458f65708b4ad869e7c594555b6f44440
dishantraut <63045109+dishantraut@users.noreply.github.com>
2020-11-26 16:14:38 +0530

    01_Hello_World

---
+++ 01_Hello_World.go
@@ -0,0 +1,13 @@
+
+// The First line is the package deceleration, here the name of the package is main.
+// Package deceleration is mandatory for all the go programs.
+package main
+
+
+// The compiler will include the files of the package fmt.
+import ("fmt")
+
+
+func main() {
+    fmt.Println("Hello World")
+}

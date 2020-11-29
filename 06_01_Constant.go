80d6368194f96258a678364178b923bbb25316f0
dishantraut <63045109+dishantraut@users.noreply.github.com>
2020-11-29 17:22:55 +0530

    Constant

---
+++ 06_01_Constant.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  // constant
+  const Height int = 100
+  const Width int = 200
+  var area int
+  area = Height * Width
+  fmt.Println("Area = ",area)
+}

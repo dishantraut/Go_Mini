05316ae1b3395804b0d2768f81a4bde089df91df
dishantraut <63045109+dishantraut@users.noreply.github.com>
2020-11-29 19:22:47 +0530

    Array

---
+++ 09_01_Array.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+)
+
+
+func main() {
+  //int array with length 3
+  var a [3] int
+
+  var a1[3] int
+  a1[0] = 19970
+  a1[1] = 1998
+  a1[2] = 1999
+
+  a2 := [3]string{"Dishant", "Rajendra", "Raut"}
+
+  fmt.Println(a)
+  fmt.Println(a1)
+  fmt.Println(a2)
+}
